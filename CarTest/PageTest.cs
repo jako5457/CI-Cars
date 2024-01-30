68219e5ebbf1855be9cd46175af837dff25a6997
Jakob spiel <jako5457@gmail.com>
2024-01-30 08:57:30 +0100

    ok

--- CarTest/PageTest.cs
+++ CarTest/PageTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
 
 namespace CarTest
 {
@@ -25,7 +26,6 @@ namespace CarTest
 
             //Assert
             Assert.Equal(ExpectedTitle, webDriver.Title);
-            webDriver.Close();
         }
 
         [Fact]
@@ -43,13 +43,13 @@ namespace CarTest
             var SubmitButton = webDriver.FindElement(By.XPath("/html/body/div/main/form/button"));
 
             CarNameBox.SendKeys("Small Car");
+
             CarTypeBox.SendKeys("Pedal");
 
             SubmitButton.Click();
 
             //Assert
             Assert.Equal(Address, webDriver.Url);
-            webDriver.Close();
         }
 
     }
